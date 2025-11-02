f5e19d57ed43a55e80b944663010edd71ca214fd
lithincg <lithinraju1@gmail.com>
2025-11-02 17:15:06 +0530

    The initial Spring Boot project structure

---
+++ src/main/java/com/lithin/microstock_manager/MicrostockManagerApplication.java
@@ -0,0 +1,13 @@
+package com.lithin.microstock_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrostockManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrostockManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lithin/microstock_manager/MicrostockManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lithin.microstock_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrostockManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
